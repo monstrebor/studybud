20204ca055e230be853ff2c7800bf0affa760f73
Robert Janssen T. Campos <crobertjanssen@gmail.com>
2025-05-05 01:01:02 +0800

    django 7hrs tutorial completed

--- base/forms.py
+++ base/forms.py
@@ -1,6 +1,14 @@
 from django.forms import ModelForm
-from .models import Room
-from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from .models import Room, User
+
+
+
+
+class MyUserCreationForm(UserCreationForm):
+    class Meta: 
+        model = User 
+        fields =['name', 'username', 'email', 'password1', 'password2']
 
 class RoomForm(ModelForm):
     class Meta:
@@ -8,7 +16,9 @@ class RoomForm(ModelForm):
         fields = '__all__'
         exclude = ['host','participants']
 
+
 class UserForm(ModelForm):
     class Meta:
         model = User
-        fields = ['username', 'email']
+        fields = ['avatar','name','username', 'email','bio']
+
